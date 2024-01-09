acf934cc269cfbae9faf65fa0a4a51864c59f8f1
hmyunis <hamdim801@gmail.com>
2024-01-09 13:10:36 +0300

    Made the Browse page get populated from the DB.

--- frontend/public/js/displayAllGames.ts
+++ frontend/public/js/displayAllGames.ts
@@ -1,21 +1,22 @@
-interface GameAttrs {
-    id: number;
-    title: string;
-    description: string;
-    genre: string;
-    platform: string;
-    releaseDate: string;
-}
+// interface GameAttrs {
+//     id: number;
+//     title: string;
+//     description: string;
+//     genre: string;
+//     platform: string;
+//     releaseDate: string;
+// }
 
-interface DisplayableGameAttrs {
-    id: number;
-    imgSrc: string;
-    title: string;
-    releaseDate: string;
-}
-const browseContainer = document.getElementById('browse-game-container');
+// interface DisplayableGameAttrs {
+//     id: number;
+//     imgSrc: string;
+//     title: string;
+//     releaseDate: string;
+// }
+const browseContainer = document.getElementById('browse-games-container');
+const GAME_API_URL = 'http://localhost:3000/games';
 
-fetch('https://localhost:3000/games')
+fetch(GAME_API_URL)
     .then((response) => response.json())
     .then((data) => {
         data.forEach((game) => {
@@ -24,40 +25,39 @@ fetch('https://localhost:3000/games')
     })
     .catch((error) => console.error('ERROR: ', error));
 
-function insertGameItemToHTML(displayableGameAttrs: DisplayableGameAttrs) {
-    const { id, imgSrc, title, releaseDate } = displayableGameAttrs;
+function insertGameItemToHTML(gameObj) {
+    const { id, title, releaseDate } = gameObj;
     const gameItemTemplate = `
-    <div id="game-${id}" class="w-72 m-5 text-white rounded-xl bg-slate-900 hover:-translate-y-3 transition-all duration-300">
-    <img src="${imgSrc}" alt="${title}" class="rounded-t-xl">
-    <section class="p-3">
-        <hgroup>
-            <h3 class="text-3xl" id="game-title">${title}</h3>
-            <h5 class="text-sm text-slate-500" id="game-release-date">${releaseDate}</h5>
-        </hgroup>
-        <div class="flex justify-between mt-1">
-            <button class="text-sm text-blue-500" id="game-details" onclick="displayDetails(${id});">Details</button>
-            <section class="flex gap-3">
-                <select id="add-to-collection" class="text-black bg-[#1efe80] rounded">
-                    <option value="" disabled selected>Add to</option>
-                    <option value="played">Played</option>
-                    <option value="playing">Playing</option>
-                    <option value="planned">Planned</option>
-                </select>
-                <button class="bg-blue-400 p-1 px-2 rounded hover:bg-white hover:text-blue-600">Save</button>
-            </section>
-        </div>
-    </section>
+    <div id="game-id-${id}" class="w-72 m-5 text-white rounded-xl bg-slate-900 hover:-translate-y-3 transition-all duration-300">
+        <img src="images/avatar.jpg" alt="${title}" class="rounded-t-xl">
+        <section class="p-3">
+            <hgroup>
+                <h3 class="text-3xl" id="game-title">${title}</h3>
+                <h5 class="text-sm text-slate-500" id="game-release-date">${releaseDate}</h5>
+            </hgroup>
+            <div class="flex justify-between mt-1">
+                <button class="text-sm text-blue-500" id="game-${id}-details" onclick="displayDetails(${id});">Details</button>
+                <section class="flex gap-3">
+                    <select id="add-to-collection" class="text-black bg-[#1efe80] rounded">
+                        <option value="" disabled selected>Add to</option>
+                        <option value="played">Played</option>
+                        <option value="playing">Playing</option>
+                        <option value="planned">Planned</option>
+                    </select>
+                    <button id="game-${id}-save" class="bg-blue-400 p-1 px-2 rounded hover:bg-white hover:text-blue-600">Save</button>
+                </section>
+            </div>
+        </section>
     </div>
     `;
     browseContainer?.insertAdjacentHTML('beforeend', gameItemTemplate);
 }
 
-
 function getGameObjById(gameId: number) {
-    fetch(`http://localhost:3000/games/${gameId}`)
+    fetch(GAME_API_URL + `/${gameId}`)
         .then((response) => response.json())
         .then((data) => {
-            return data[0];
+            console.log(data);
         })
         .catch((error) => console.error('ERROR: ', error));
 }
