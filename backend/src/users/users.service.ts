22f0df2780524937556010bef6164c7f294a74ac
Estifanos Taye <estifanostaye11@gmail.com>
2024-01-10 20:57:50 +0300

    some fixes

--- backend/src/users/users.service.ts
+++ backend/src/users/users.service.ts
@@ -5,12 +5,8 @@ import { InjectRepository } from '@nestjs/typeorm';
 
 @Injectable()
 export class UsersService {
-<<<<<<< HEAD
   constructor(@InjectRepository(User) private repo: Repository<User>) {}
-=======
-  constructor(@InjectRepository(User) private repo: Repository<User>,
-  private collectionsService: CollectionsService){}
->>>>>>> 83d783c5879dbcb71a83a5ea4e04e0463c97c604
+
   create(username: string, password: string) {
     const user = this.repo.create({ username, password });
     return this.repo.save(user);
