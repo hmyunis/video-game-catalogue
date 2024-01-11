bf1acf403d061e89c14cc0d85ebbdfb04799272c
motilegge <motileggese@gmail.com>
2024-01-11 20:58:36 +0300

    added testing to authentication service

--- backend/src/auth/auth.service.spec.ts
+++ backend/src/auth/auth.service.spec.ts
@@ -1,10 +1,15 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AuthService } from './auth.service';
+import { UsersService } from '../users/users.service';
+import { User } from 'src/users/user.entity';
 
 describe('AuthService', () => {
   let service: AuthService;
+  let fakeusersService: Partial<UsersService>;
 
   beforeEach(async () => {
+    // create a fake copy of the users service
+    
     const module: TestingModule = await Test.createTestingModule({
       providers: [AuthService],
     }).compile();
--- backend/src/users/users.controller.spec.ts
+++ backend/src/users/users.controller.spec.ts
@@ -1,5 +1,8 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UsersController } from './users.controller';
+import { UsersService } from './users.service';
+import { AuthService } from 'src/auth/auth.service';
+import { User } from './user.entity';
 
 describe('UsersController', () => {
   let controller: UsersController;
