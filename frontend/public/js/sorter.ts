6762f31adcbcba7a4a20b413a0dbefecf075ea90
beleir101 <beleirmessay2436@gmail.com>
2024-01-11 21:39:03 +0300

    tried to update the search ts half way

--- frontend/public/js/sorter.ts
+++ frontend/public/js/sorter.ts
@@ -18,11 +18,20 @@ const APILINK =
 const IMG_PATH = "https://image.tmdb.org/t/p/w1280";
 const SEARCHAPI =
   "https://api.themoviedb.org/3/search/movie?&api_key=41ee980e4b5f05f6693fda00eb7c4fd4&query=";
+  const action: = document.getElementById("sort-by-action");
+  const shooter = document.getElementById("sort-by-shooter");
+  const Horror = document.getElementById("sort-by-horror");
+  const Fighting = document.getElementById("sort-by-fighting");
+  const Anime = document.getElementById("sort-by-anime");
+  const RPG = document.getElementById("sort-by-rpg");
+  const Strategy = document.getElementById("sort-by-strategy");
+  const Racing = document.getElementById("sort-by-racing");
+  const main = document.getElementById("browse-games-container");
+  const Sport = document.getElementById("sort-by-sports");
 
-const browseContainer = document.getElementById("browse-games-container");
-const GAME_API_URL = "http://localhost:3000/games";
+import { insertGameItemToHTML } from "./displayAllGames";
 
-fetch(GAME_API_URL)
+fetch(APILINK)
   .then((response) => response.json())
   .then((data) => {
     data.forEach((game) => {
@@ -31,39 +40,51 @@ fetch(GAME_API_URL)
   })
   .catch((error) => console.error("ERROR: ", error));
 
-function insertGameItemToHTML(gameObj) {
-  const { id, title, releaseDate, imageUrl } = gameObj;
-  const gameItemTemplate = `
-    <div id="game-id-${id}" class="w-72 h-1/4 m-5 text-white rounded-xl bg-slate-900 hover:-translate-y-3 transition-all duration-300">
-        <img src="${imageUrl}" alt="${title}" class="rounded-t-xl h-96 w-full">
-        <section class="p-3 h-fit w-full">
-            <hgroup>
-                <h3 class="text-3xl" id="game-title">${title}</h3>
-                <h5 class="text-sm text-slate-500" id="game-release-date">${releaseDate}</h5>
-            </hgroup>
-            <div class="flex justify-between mt-1">
-                <button class="text-sm text-blue-500" id="game-${id}-details" onclick="displayDetails(${id});">Details</button>
-                <section class="flex gap-3">
-                    <select id="add-to-collection-for-${id}" class="text-black bg-[#1efe80] rounded" onchange="activation(event.target.value)">
-                        <option value="" disabled selected>Add to</option>
-                        <option value="played">Played</option>
-                        <option value="playing">Playing</option>
-                        <option value="planned">Planned</option>
-                    </select>
-                    <button id="game-${id}-save" onclick="saveToCollection(${id})" class="bg-blue-400 p-1 px-2 rounded hover:bg-white hover:text-blue-600">Save</button>
-                </section>
-            </div>
-        </section>
-    </div>
-    `;
-  browseContainer?.insertAdjacentHTML("beforeend", gameItemTemplate);
-}
+  
+action.addEventListener("click", (e) => {
+    e.preventDefault();
+    main.innerHTML = "";
+    game(SEARCHAPI + "Action");
+  });
+  Strategy.addEventListener("click", (e) => {
+    e.preventDefault();
+    main.innerHTML = "";
+    game(SEARCHAPI + "Strategy");
+  });
+  Racing.addEventListener("click", (e) => {
+    e.preventDefault();
+    main.innerHTML = "";
+    game(SEARCHAPI + "Racing");
+  });
+  RPG.addEventListener("click", (e) => {
+    e.preventDefault();
+    main.innerHTML = "";
+    game(SEARCHAPI + "RPG");
+  });
+  Anime.addEventListener("click", (e) => {
+    e.preventDefault();
+    main.innerHTML = "";
+    game(SEARCHAPI + "Anime");
+  });
+  Fighting.addEventListener("click", (e) => {
+    e.preventDefault();
+    main.innerHTML = "";
+    game(SEARCHAPI + "Fighting");
+  });
+  Horror.addEventListener("click", (e) => {
+    e.preventDefault();
+    main.innerHTML = "";
+    game(SEARCHAPI + "Horror");
+  });
+  shooter.addEventListener("click", (e) => {
+    e.preventDefault();
+    main.innerHTML = "";
+    game(SEARCHAPI + "shooter");
+  });
+  
+  Sport.addEventListener("click", (e) => {
+    e.preventDefault();
+    main.innerHTML = "";
+    game(SEARCHAPI + "Sport");
+  });
 
-function getGameObjById(gameId: number) {
-  fetch(GAME_API_URL + `/${gameId}`)
-    .then((response) => response.json())
-    .then((data) => {
-      console.log(data);
-    })
-    .catch((error) => console.error("ERROR: ", error));
-}
