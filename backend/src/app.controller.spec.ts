74928a16526712b72e86381a33ffa3a46735a1bb
motilegge <motileggese@gmail.com>
2024-01-12 21:19:30 +0300

    added the tests for app.controller

--- backend/src/app.controller.spec.ts
+++ backend/src/app.controller.spec.ts
@@ -1,22 +1,55 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { JwtService } from '@nestjs/jwt';
 
 describe('AppController', () => {
   let appController: AppController;
+  let appService: AppService;
 
   beforeEach(async () => {
     const app: TestingModule = await Test.createTestingModule({
       controllers: [AppController],
-      providers: [AppService],
+      providers: [
+        AppService,
+        { provide: JwtService, useValue: {} }, // provide JwtService
+      ],
     }).compile();
 
     appController = app.get<AppController>(AppController);
+    appService = app.get<AppService>(AppService);
   });
 
-  describe('root', () => {
-    it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Hello World!');
+  describe('getHello', () => {
+    it('should call appService.getHello', () => {
+      const req = {}; // Mock req object
+      const getHelloSpy = jest
+        .spyOn(appService, 'getHello')
+        .mockImplementation(() => '');
+
+      appController.getHello(req);
+
+      expect(getHelloSpy)
+    });
+  });
+
+  describe('getout', () => {
+    it('should do something with res', () => {
+      const res = { clearCookie: jest.fn() }; // Mock res object
+
+      appController.getout(res);
+
+      expect(res.clearCookie).toHaveBeenCalled();
+    });
+  });
+
+  describe('getin', () => {
+    it('should do something with res', () => {
+      const res = { cookie: jest.fn() }; // Mock res object
+
+      appController.getin(res);
+
+      expect(res.cookie).toHaveBeenCalled();
     });
   });
 });
