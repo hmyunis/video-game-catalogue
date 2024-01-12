b6193fd127abab7363c954584152bdfca30cf728
beleir101 <beleirmessay2436@gmail.com>
2024-01-12 10:09:46 +0300

    removed duplicate functions

--- backend/src/auth/auth.service.ts
+++ backend/src/auth/auth.service.ts
@@ -33,24 +33,6 @@ export class AuthService {
     return await this.jwtService.signAsync(payload);
   }
 
-  async createUser(username: string, password: string) {
-    // Check if the username is already in use
-    const users = await this.usersService.find(username);
-    if (users.length) {
-      throw new BadRequestException('username in use');
-    }
-  
-    // Create a salt and hash the password
-    const salt = randomBytes(8).toString('hex');
-    const hash = (await scrypt(password, salt, 32)) as Buffer;
-  
-    // Combine the salt and the hash to store in the database
-    const result = salt + '.' + hash.toString('hex');
-  
-    // Create the user and return the result
-    return await this.usersService.create(username, result);
-  }
-
   async signIn(username: string, password: string) {
     const [user] = await this.usersService.find(username);
     if (!user) {
