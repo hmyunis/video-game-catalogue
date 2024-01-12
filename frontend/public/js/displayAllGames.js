7e43987a6926b225722bebbb72fff6e53a6d588e
hmyunis <hamdim801@gmail.com>
2024-01-12 03:43:43 +0300

    Frontend of home.html is done. Needs backend fixes

--- frontend/public/js/displayAllGames.js
+++ frontend/public/js/displayAllGames.js
@@ -1,37 +1,15 @@
-// interface GameAttrs {
-//     id: number;
-//     title: string;
-//     description: string;
-//     genre: string;
-//     platform: string;
-//     releaseDate: string;
-// }
-// interface DisplayableGameAttrs {
-//     id: number;
-//     imgSrc: string;
-//     title: string;
-//     releaseDate: string;
-// }
 var browseContainer = document.getElementById('browse-games-container');
-var GAME_API_URL = 'http://localhost:3000/games';
-fetch(GAME_API_URL)
+fetch('http://localhost:3000/games')
     .then(function (response) { return response.json(); })
     .then(function (data) {
     data.forEach(function (game) {
-        insertGameItemToHTML(game);
+        insertGameItemsToHTML(game);
     });
 })
     .catch(function (error) { return console.error('ERROR: ', error); });
-function insertGameItemToHTML(gameObj) {
+function insertGameItemsToHTML(gameObj, userId) {
+    if (userId === void 0) { userId = 3; }
     var id = gameObj.id, title = gameObj.title, releaseDate = gameObj.releaseDate, imageUrl = gameObj.imageUrl;
-    var gameItemTemplate = "\n    <div id=\"game-id-".concat(id, "\" class=\"w-72 h-1/4 m-5 text-white rounded-xl bg-slate-900 hover:-translate-y-3 transition-all duration-300\">\n        <img src=\"").concat(imageUrl, "\" alt=\"").concat(title, "\" class=\"rounded-t-xl h-96 w-full\">\n        <section class=\"p-3 h-fit w-full\">\n            <hgroup>\n                <h3 class=\"text-3xl\" id=\"game-title\">").concat(title, "</h3>\n                <h5 class=\"text-sm text-slate-500\" id=\"game-release-date\">").concat(releaseDate, "</h5>\n            </hgroup>\n            <div class=\"flex justify-between mt-1\">\n                <button class=\"text-sm text-blue-500\" id=\"game-").concat(id, "-details\" onclick=\"displayDetails(").concat(id, ");\">Details</button>\n                <section class=\"flex gap-3\">\n                    <select id=\"add-to-collection-for-").concat(id, "\" class=\"text-black bg-[#1efe80] rounded\" onchange=\"activation(event.target.value)\">\n                        <option value=\"\" disabled selected>Add to</option>\n                        <option value=\"played\">Played</option>\n                        <option value=\"playing\">Playing</option>\n                        <option value=\"planned\">Planned</option>\n                    </select>\n                    <button id=\"game-").concat(id, "-save\" onclick=\"saveToCollection(").concat(id, ")\" class=\"bg-blue-400 p-1 px-2 rounded hover:bg-white hover:text-blue-600\">Save</button>\n                </section>\n            </div>\n        </section>\n    </div>\n    ");
+    var gameItemTemplate = "\n    <div id=\"game-id-".concat(id, "\" class=\"w-72 h-1/4 m-5 text-white rounded-xl bg-slate-900 hover:-translate-y-3 transition-all duration-300\">\n        <img src=\"").concat(imageUrl, "\" alt=\"").concat(title, "\" class=\"rounded-t-xl h-96 w-full\">\n        <section class=\"p-3 h-fit w-full\">\n            <hgroup>\n                <h3 class=\"text-3xl\" id=\"game-title\">").concat(title, "</h3>\n                <h5 class=\"text-sm text-slate-500\" id=\"game-release-date\">").concat(releaseDate, "</h5>\n            </hgroup>\n            <div class=\"flex justify-between mt-1\">\n                <button class=\"text-sm text-blue-500\" id=\"game-").concat(id, "-details\" onclick=\"displayDetails(").concat(id, ");\">Details</button>\n                <section class=\"flex gap-3\">\n                    <select id=\"add-to-collection-for-").concat(userId, "\" class=\"text-black bg-[#1efe80] rounded\" onchange=\"activation(event.target.value)\">\n                        <option value=\"\" disabled selected>Add to</option>\n                        <option value=\"played\">Played</option>\n                        <option value=\"playing\">Playing</option>\n                        <option value=\"planned\">Planned</option>\n                    </select>\n                    <button id=\"game-").concat(id, "-save\" onclick=\"saveToCollection(").concat(id, ")\" class=\"bg-blue-400 p-1 px-2 rounded hover:bg-white hover:text-blue-600\">Save</button>\n                </section>\n            </div>\n        </section>\n    </div>\n    ");
     browseContainer === null || browseContainer === void 0 ? void 0 : browseContainer.insertAdjacentHTML('beforeend', gameItemTemplate);
-}
-function getGameObjById(gameId) {
-    fetch(GAME_API_URL + "/".concat(gameId))
-        .then(function (response) { return response.json(); })
-        .then(function (data) {
-        console.log(data);
-    })
-        .catch(function (error) { return console.error('ERROR: ', error); });
 }
---
+++ frontend/public/js/displayGameDetails.js
@@ -0,0 +1,29 @@
+async function displayDetails(gameId) {
+    const response = await fetch(`http://localhost:3000/games/${gameId}`);
+    const gameObj = await response.json();
+    const { title, description, genre, platform, publisher, releaseDate, imageUrl } = gameObj;
+    const modalHTML = `
+    <div id="details-modal" class="fixed p-10 top-[50%] left-[50%] max-md:w-[95vw] translate-x-[-50%] translate-y-[-45%] h-[80vh] w-[80vw] rounded-xl shadow-2xl bg-slate-400 text-gray-800">
+        <div class="w-full h-full flex max-md:flex-col max-md:overflow-y-scroll">
+            <picture class="w-1/2 h-full max-md:w-full">
+            <img src="${imageUrl}" alt="ac-origins" class="w-full h-full">
+            </picture>
+            <img id="close-modal-icon" src="images/svg/close-icon.svg" class="top-3 right-5 w-8 hover:scale-125 hover:cursor-pointer transition-all fixed" alt="Close">
+            <section class="w-1/2 px-8">
+            <div class="text-xl">
+                <p class="my-12"><span class="text-2xl mr-3 font-bold">Name:</span>${title}</p>
+                <p class="my-12"><span class="text-2xl mr-3 font-bold">Genre:</span>${genre}</p>
+                <p class="my-12"><span class="text-2xl mr-3 font-bold">Release Date:</span>${releaseDate}</p>
+                <p class="my-12"><span class="text-2xl mr-3 font-bold">Platforms:</span>${platform}</p>
+                <p class="my-12"><span class="text-2xl mr-3 font-bold">Publisher:</span>${publisher}</p>
+                <p class="my-12"><span class="text-2xl mr-3 font-bold">Description:</span>${description}</p>
+            </div>
+            </section>
+        </div>
+    </div>
+    `;
+    document.body.insertAdjacentHTML('afterbegin', modalHTML);
+    const closeIcon = document.getElementById('close-modal-icon');
+    const modal = document.getElementById('details-modal');
+    closeIcon.addEventListener('click', () => modal.classList.add('hidden'));
+}
